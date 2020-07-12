cd5bb5144e1c52750375494bf879babb3341546d
Nathachanok Netmaneesuk <nathachanok.n@gmail.com>
2020-07-11 20:01:48 -0700

    Components part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,18 +1,34 @@
 import React, { Component } from 'react';
+import { View } from 'react-native';
 import Menu from './MenuComponent';
 import { DISHES } from '../shared/dishes';
+import Dishdetail from './DishdetailComponent';
 
 export default class Main extends Component {
+
     constructor(props) {
         super(props);
         this.state = {
-            dishes: DISHES
+            dishes: DISHES,
+            selectedDish: null
         }
+    }
+
+    onDishSelect(dishID) {
+        this.setState({selectedDish: dishID});
     }
 
     render() {
         return(
-            <Menu dishes={this.state.dishes} />
+            <View style={{flex: 1}}>
+                <Menu 
+                    dishes={this.state.dishes} 
+                    onPress={(dishID) => this.onDishSelect(dishID)}
+                    />
+                <Dishdetail 
+                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]} 
+                    />
+            </View>
         );
     }
 }
--- components/MenuComponent.js
+++ components/MenuComponent.js
@@ -7,20 +7,21 @@ export default function Menu(props) {
     const renderMenuItem = ({item, index}) => {
         return(
             <ListItem
-                key = {index}
-                title = {item.name}
-                subtitle = {item.description}
-                hideChevron = {true}
-                leftAvatar = {{ source: require('./images/uthappizza.png')}}
+                key={index}
+                title={item.name}
+                subtitle={item.description}
+                hideChevron={true}
+                onPress={() => props.onPress(item.id)}
+                leftAvatar={{ source: require('./images/uthappizza.png')}}
                 />
         )
     }
 
     return(
         <FlatList
-            data = {props.dishes}
-            renderItem = {renderMenuItem}
-            keyExtractor = {item => item.id.toString()}
+            data={props.dishes}
+            renderItem={renderMenuItem}
+            keyExtractor={item => item.id.toString()}
             />
     )
 }
